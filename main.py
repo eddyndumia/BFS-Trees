9a25a3c6e175e41a33daa2829986a91744fbc14b
Eddy Ndumia <ndumiaeddy@gmail.com>
2023-02-02 20:34:54 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+
+# binary trees simple
+
+class Node:
+    def __init__(self, data):
+        self.right = None
+        self.left = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data is None:
+            self.data = data
+        else:
+            if data < self.data:
+                if self.left is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    # inserting recursively
+                    self.right.insert(data)
+
+root = Node("z")
+root.insert('a')
+root.insert('b')
+root.insert('c')
+root.insert('d')
+root.insert('e')
+
